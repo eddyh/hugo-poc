7129c16db2e1a3c2701fc621d69cca4afa698cd7
Edwin Westerhoud <edwinwesterhoud@voormedia.com>
2019-03-30 12:27:07 +0100

    Forestry config for browser sync.

---
+++ browser-sync.js
@@ -0,0 +1,10 @@
+module.exports = {
+  proxy: "localhost:34433",
+  listen: "0.0.0.0",
+  port: 8080,
+  open: false,
+  files: "public/**/*.*",
+  socket: {
+    domain: "http://gbtmy5h4wroexw.instant.forestry.io"
+  }
+}
